ebf267f616817cd44777e3ddd55d655d491600e0
KaranUpneja <42024629+KaranUpneja@users.noreply.github.com>
2018-08-07 17:23:05 +0530

    Update User.java

--- OnlineBankingApplication/src/com/cg/onlinebanking/dto/User.java
+++ OnlineBankingApplication/src/com/cg/onlinebanking/dto/User.java
@@ -1,9 +1,48 @@
-package com.cg.onlinebanking.dto;
+package com.cg.onlinebanking.bean;
 
 public class User 
 {
-	private int accountId;
-	private int userid;
+   private String userId;
+   private String password;
+   private String secretQuestion;
+   private String transactionPassword;
+   private String lockStatus;
+public String getUserId() {
+	return userId;
+}
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getSecretQuestion() {
+	return secretQuestion;
+}
+public void setSecretQuestion(String secretQuestion) {
+	this.secretQuestion = secretQuestion;
+}
+public String getTransactionPassword() {
+	return transactionPassword;
+}
+public void setTransactionPassword(String transactionPassword) {
+	this.transactionPassword = transactionPassword;
+}
+public String getLockStatus() {
+	return lockStatus;
+}
+public void setLockStatus(String lockStatus) {
+	this.lockStatus = lockStatus;
+}
+@Override
+public String toString() {
+	return "User [userId=" + userId + ", password=" + password + ", secretQuestion=" + secretQuestion
+			+ ", transactionPassword=" + transactionPassword + ", lockStatus=" + lockStatus + "]";
+}
+   
 	
 
 }
