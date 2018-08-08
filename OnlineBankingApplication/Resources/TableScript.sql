ab8591272cd6a539fae5181fbe97a04917f793ff
IB-0495 <42024639+IB-0495@users.noreply.github.com>
2018-08-08 17:08:37 +0530

    Update TableScript.sql

--- OnlineBankingApplication/Resources/TableScript.sql
+++ OnlineBankingApplication/Resources/TableScript.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Account_Master(Account_ID NUMBER(10) PRIMARY KEY,Account_Type VARCHAR2(25),Account_Balance NUMBER(15),Open_Date DATE, user_id VARCHAR2(20) REFERENCES User_Table(user_id), Customer_ID NUMBER(10) REFERENCES Customer(Customer_ID));
+CREATE TABLE Account_Master(Account_ID NUMBER(10) PRIMARY KEY,Account_Type VARCHAR2(25),Account_Balance NUMBER(15),Open_Date DATE,Customer_ID NUMBER(10) REFERENCES Customer(Customer_ID));
 
 CREATE TABLE Customer(Customer_ID NUMBER(10) PRIMARY KEY, customer_name VARCHAR2(50), Email VARCHAR2(30), Address VARCHAR2(100), Pancard VARCHAR2(15),user_id VARCHAR2(20) REFERENCES User_Table(user_id));
 
@@ -6,7 +6,7 @@ CREATE TABLE Transaction(Transaction_ID NUMBER(10) PRIMARY KEY,Tran_description 
 
 CREATE TABLE Service_Tracker(Service_ID NUMBER(10) PRIMARY KEY, Service_Description VARCHAR2(100),Account_ID NUMBER(20) REFERENCES Account_Master(Account_ID), Service_Raised_Date DATE ,Service_status VARCHAR2(20));
 
-CREATE TABLE User_Table(user_id VARCHAR2(20) PRIMARY KEY,login_password VARCHAR2(15),Customer_ID NUMBER REFERENCES Customer(Customer_ID),secret_question VARCHAR2(50),Transaction_password VARCHAR2(15),lock_status VARCHAR2(1));
+CREATE TABLE User_Table(user_id VARCHAR2(20) PRIMARY KEY,login_password VARCHAR2(15),secret_question VARCHAR2(50),Transaction_password VARCHAR2(15),lock_status VARCHAR2(1));
 
 CREATE TABLE Fund_Transfer(FundTransfer_ID NUMBER PRIMARY KEY,Account_ID NUMBER(10) REFERENCES Account_Master(Account_ID) ,Payee_Account_ID NUMBER(10), Date_Of_Transfer DATE, Transfer_Amount NUMBER(15));
 
