a225fc1b71625c7200efc2a1d9a87e583e9434ca
mkumarh <42026340+mkumarh@users.noreply.github.com>
2018-08-08 18:19:09 +0530

    Update OnlineBankingServiceImpl.java

--- OnlineBankingApplication/src/com/cg/onlinebanking/service/OnlineBankingServiceImpl.java
+++ OnlineBankingApplication/src/com/cg/onlinebanking/service/OnlineBankingServiceImpl.java
@@ -86,6 +86,110 @@ public class OnlineBankingServiceImpl implements IOnlineBankingService {
 		// TODO Auto-generated method stub
 		return obs.createNewAccountNewCustomer(a, c, u);
 	}
+	
+	
+	/*Account DTO validations*/
+	private void isValidAccountDetail(Account details) throws OnlineBankingException {
+		List<String> validationErrors = new ArrayList<>();
+		
+		
+		String regexuserid =  "^[A-Z][a-z][0-9]{8}$";
+		
+		if ( !isValidDetail( details.getAccountbalance() ) )
+			validationErrors.add("Account Balance should be within the bank limit.");
+		
+		if ( !isValidDetail( details.getUserid(), regexuserid) )
+			validationErrors.add("Invalid User Id ");
+		
+		if ( !validationErrors.isEmpty() ) {
+			//logger.error("Validation Errors: "+validationErrors);
+			throw new OnlineBankingException(""+validationErrors);
+		}
+	}
+
+	 private boolean isValidDetail(String regex, String data) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(data);
+		return matcher.matches();
+	}
+
+	private boolean isValidDetail(String accountbalance) {
+		IOnlineBankingDao IOnlineBankingDao = new OnlineBankingDaoImpl();
+		
+		if ( !isValidDetail( accountbalance, "^1[0-9]{3}$" ) ) {
+			IOnlineBankingDao = null;
+		}
+		return false;	
+	}
+	
+	private boolean isValidDetail1(String accounttype) {
+		if (accounttype.equalsIgnoreCase("Savings") || accounttype.equalsIgnoreCase("Current") )
+        {
+             return true;
+        }
+		else 
+	    return false;
+		}
+	
+	/*Customer dto validations done here*/
+	private void isValidCustomerDetail(Customer details) throws OnlineBankingException {
+		List<String> validationErrors = new ArrayList<>();
+		
+		String regexcustomerName =  "^[A-Z][a-z]{20}$";
+		String regexemail = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
+		String regexaddress =  "^[#.0-9{4}[a-zA-Z]\\s,-]+$";
+		String regexpanCard =  "^[A-Za-z]{5}\\d{4}[A-Za-z]{1}$";
+		
+		if ( !isValidDetail( details.getCustomerName(), regexcustomerName) )
+			validationErrors.add("Customer Name is incorrect.");
+		
+		if ( !isValidDetail( details.getEmail(), regexemail) )
+			validationErrors.add("email is incorrect.");
+		
+		if ( !isValidDetail( details.getAddress(), regexaddress) )
+			validationErrors.add("Customer Address is incorrect.");
+		
+		if ( !isValidDetail( details.getPanCard(), regexpanCard) )
+			validationErrors.add("PanCard you entered is incorrect.");
+		
+		if ( !validationErrors.isEmpty() ) {
+			//logger.error("Validation Errors: "+validationErrors);
+			throw new OnlineBankingException(""+validationErrors);
+		}
+	}
+
+	/*User Dto validations are done here*/
+	private void isValidUserDetail(User details) throws OnlineBankingException {
+		List<String> validationErrors = new ArrayList<>();
+		
+		String regexuserid =  "^[A-Z][a-z][0-9]{8}$";
+		String regexpassword = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+		String regextransactionPassword =  "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+				
+		if ( !isValidDetail( details.getUserId(), regexuserid) )
+			validationErrors.add("Customer UserId is incorrect.");
+		
+		if ( !isValidDetail( details.getPassword(), regexpassword) )
+			validationErrors.add("password is incorrect.");
+		
+		if ( !isValidDetail( details.getTransactionPassword(), regextransactionPassword) )
+			validationErrors.add("Customer Transaction password is incorrect.");
+		
+		
+		if ( !validationErrors.isEmpty() ) {
+			//logger.error("Validation Errors: "+validationErrors);
+			throw new OnlineBankingException(""+validationErrors);
+		}
+	}
+	
+	private boolean isValidpassword(String password,String transactionPassword) {
+		if (!password.equals(transactionPassword))
+        {
+             return true;
+        }
+		else 
+	    return false;
+		}
 
 	
 
