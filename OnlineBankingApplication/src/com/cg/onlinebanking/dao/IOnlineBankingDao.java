a176b7eabb78eb56adf6e6f4ea452de2a8972d76
IB-0495 <42024639+IB-0495@users.noreply.github.com>
2018-08-08 17:01:54 +0530

    Update IOnlineBankingDao.java

--- OnlineBankingApplication/src/com/cg/onlinebanking/dao/IOnlineBankingDao.java
+++ OnlineBankingApplication/src/com/cg/onlinebanking/dao/IOnlineBankingDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.cg.onlinebanking.dto.Account;
 import com.cg.onlinebanking.dto.Customer;
 import com.cg.onlinebanking.dto.Transactions;
+import com.cg.onlinebanking.dto.User;
 import com.cg.onlinebanking.exception.OnlineBankingException;
 
 public interface IOnlineBankingDao {
@@ -13,13 +14,14 @@ public interface IOnlineBankingDao {
 	public List<Transactions> viewMiniStatement()throws OnlineBankingException;
 	public String viewCurrentAddressMobile()throws OnlineBankingException;
 	public String trackServiceRequest()throws OnlineBankingException;
-	public int createNewAccount(Account a,Customer c)throws OnlineBankingException;
 	public List<Transactions> viewAllTransctions()throws OnlineBankingException;
 	public List<Transactions> viewDetailStatement(LocalDate from, LocalDate to)throws OnlineBankingException;
-	public boolean changeAddressMobile(String address)throws OnlineBankingException;
 	public boolean transferFundDiffAcc(int from, int to)throws OnlineBankingException;
 	public boolean transferFundOwnAcc(int from, int to)throws OnlineBankingException;
 	public boolean changePassword(String pass)throws OnlineBankingException;
+	public boolean changeAddressMobile(Customer c) throws OnlineBankingException;
+	public int createNewAccountExistingCustomer(Account a, int customerId) throws OnlineBankingException;
+	public int createNewAccountNewCustomer(Account a, Customer c, User u) throws OnlineBankingException;
 
 
 }
